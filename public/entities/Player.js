dd0e9c8d10145b32c50ef412fa831f6f6fddf084
Steven Kane <stevenkane@Stevens-MacBook-Pro.local>
2014-06-04 14:17:57 -0500

    added basic jumping constructs to the player object

--- public/entities/Player.js
+++ public/entities/Player.js
@@ -3,11 +3,14 @@ module.exports = class Player extends Phaser.Sprite {
     super(state, x, y, "player")
     state.physics.arcade.enable(this)
     this.body.collideWorldBounds = true
+    this.body.setSize(48, 48)
     this.checkWorldBounds = true
     this.scale.x = .25
     this.scale.y = .25
     this.anchor.set(.5)
     this.speed = 150
+    this.jumping = false
+    this.jumpDuration = 800
   }
   stop() {
     this.body.velocity.y = 0 
@@ -25,4 +28,22 @@ module.exports = class Player extends Phaser.Sprite {
   moveRight() {
     this.body.velocity.x = this.speed
   }
+  jump() {
+    if (this.jumping) return false
+    this.jumping = true
+    this.scale.x = .5
+    this.scale.y = .5
+    setTimeout(this.land.bind(this), this.jumpDuration)
+    console.log("jumped")
+  }
+  land() {
+    this.jumping = false 
+    this.scale.x = .25
+    this.scale.y = .25
+    console.log("landed")
+  }
+}
+
+var calculateSize = (duration, airTime) => {
+   
 }
--- public/states/Round.js
+++ public/states/Round.js
@@ -14,18 +14,19 @@ module.exports = class Round extends Phaser.State {
     var rightKey = this.input.keyboard.addKey(Phaser.Keyboard.RIGHT)
     var downKey = this.input.keyboard.addKey(Phaser.Keyboard.DOWN)
     var leftKey = this.input.keyboard.addKey(Phaser.Keyboard.LEFT)
+    var jumpKey = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR)
 
+    this.physics.startSystem(Phaser.Physics.Arcade)
     this.inputs = [
       {key: upKey, fn: player1.moveUp.bind(player1)},
       {key: rightKey, fn: player1.moveRight.bind(player1)},
       {key: downKey, fn: player1.moveDown.bind(player1)},
       {key: leftKey, fn: player1.moveLeft.bind(player1)},
+      {key: jumpKey, fn: player1.jump.bind(player1)}
     ]
 
     this.players = this.add.group()
     this.walls = this.add.group()
-
-    this.physics.startSystem(Phaser.Physics.Arcade)
 
     this.players.add(player1)
     this.walls.add(wall1)
