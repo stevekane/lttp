821caef5596864a1cbfda2eb9cc90bea8ec84b96
Steven Kane <stevenkane@Stevens-MacBook-Pro.local>
2014-06-05 00:29:26 -0500

    added basic animations and sounds and rotations and a buynch of cool stuff because yes

--- public/entities/Player.js
+++ public/entities/Player.js
@@ -1,49 +1,73 @@
 module.exports = class Player extends Phaser.Sprite {
-  constructor(state, x, y) {
-    super(state, x, y, "player")
-    state.physics.arcade.enable(this)
+  constructor(game, x, y) {
+    super(game, x, y, "player", 0)
+
+    game.physics.arcade.enable(this)
     this.body.collideWorldBounds = true
-    this.body.setSize(48, 48)
     this.checkWorldBounds = true
-    this.scale.x = .25
-    this.scale.y = .25
-    this.anchor.set(.5)
+    this.anchor.setTo(.5, .5)
     this.speed = 150
     this.jumping = false
-    this.jumpDuration = 800
+    this.jumpDuration = 600
+    this.z = 1.0
+    this.scale.setTo(this.z, this.z)
+
+    this.up = false
+    this.right = false
+    this.down = false
+    this.left = false
+
+    this.animations.add("walking", [0, 1, 2, 3], 8)
+    this.animations.add("jumping", [3], 8)
+    this.animations.add("idle", [0], 8)
+    this.rotationOffset = (-1 * Math.PI / 2)
+
+    this.jumpSound = game.add.audio("jump")
+    this.landSound = game.add.audio("land")
   }
-  stop() {
-    this.body.velocity.y = 0 
-    this.body.velocity.x = 0 
-  }
-  moveUp() {
-    this.body.velocity.y = -1 * this.speed
-  }
-  moveDown() {
-    this.body.velocity.y = this.speed
-  }
-  moveLeft() {
-    this.body.velocity.x = -1 * this.speed
-  }
-  moveRight() {
-    this.body.velocity.x = this.speed
-  }
+
   jump() {
     if (this.jumping) return false
-    this.jumping = true
-    this.scale.x = .5
-    this.scale.y = .5
-    setTimeout(this.land.bind(this), this.jumpDuration)
-    console.log("jumped")
-  }
-  land() {
-    this.jumping = false 
-    this.scale.x = .25
-    this.scale.y = .25
-    console.log("landed")
-  }
-}
 
-var calculateSize = (duration, airTime) => {
-   
+    var initialHeight = this.z
+      , apex = initialHeight * 1.2
+      , upTime = this.jumpDuration / 2
+      , downTime = this.jumpDuration / 2
+      , easeUp = Phaser.Easing.Sinusoidal.Out
+      , easeDown = Phaser.Easing.Sinusoidal.In
+      , ascent = this.game.add.tween(this).to({z: apex}, upTime, easeUp)
+      , descent = this.game.add.tween(this).to({z: initialHeight}, downTime, easeDown)
+
+    ascent.onStart.add(() => {
+     this.jumpSound.play()
+     this.jumping = true
+    })
+    descent.onComplete.add(() => {
+      this.landSound.play()
+      this.jumping = false
+    })
+    ascent.chain(descent).start()
+  }
+
+  update() {
+    var leftVel = this.left ? this.speed * -1 : 0
+      , rightVel = this.right ? this.speed : 0
+      , upVel = this.up ? this.speed * -1 : 0
+      , downVel = this.down ? this.speed : 0
+      , xVel = leftVel + rightVel
+      , yVel = upVel + downVel
+      , stopped = (!xVel && !yVel)
+
+    this.scale.x = this.z
+    this.scale.y = this.z
+
+    if (!this.jumping) {
+      this.body.velocity.x = xVel 
+      this.body.velocity.y = yVel
+      this.rotation = stopped ? this.rotation : Phaser.Math.angleBetween(0, 0, xVel, yVel) + this.rotationOffset
+      this.animations.play(stopped ? "idle" : "walking")
+    } else {
+      this.animations.play("jumping") 
+    }
+  }
 }
--- public/states/Round.js
+++ public/states/Round.js
@@ -3,12 +3,14 @@ var Wall = require("../entities/Wall")
 
 module.exports = class Round extends Phaser.State {
   preload() {
-    this.load.image('player', 'sprites/player.png')
+    this.load.spritesheet('player', 'sprites/enemy.png', 64, 64, 4)
     this.load.image('wall', 'sprites/player.png')
+    this.load.audio('jump', 'sounds/jump.ogg')
+    this.load.audio('land', 'sounds/land.ogg')
   }
 
   create() {
-    var player1 = new Player(this, 0, 0)
+    var player1 = new Player(this.game, 0, 0)
     var wall1 = new Wall(this, 200, 200)
     var upKey = this.input.keyboard.addKey(Phaser.Keyboard.UP)
     var rightKey = this.input.keyboard.addKey(Phaser.Keyboard.RIGHT)
@@ -18,28 +20,51 @@ module.exports = class Round extends Phaser.State {
 
     this.physics.startSystem(Phaser.Physics.Arcade)
     this.inputs = [
-      {key: upKey, fn: player1.moveUp.bind(player1)},
-      {key: rightKey, fn: player1.moveRight.bind(player1)},
-      {key: downKey, fn: player1.moveDown.bind(player1)},
-      {key: leftKey, fn: player1.moveLeft.bind(player1)},
-      {key: jumpKey, fn: player1.jump.bind(player1)}
+      {
+        key: upKey, 
+        down: () => { player1.up = true}, 
+        up: () => {player1.up = false}
+      },
+      {
+        key: rightKey, 
+        down: () => { player1.right = true}, 
+        up: () => {player1.right = false}
+      },
+      {
+        key: downKey, 
+        down: () => { player1.down = true}, 
+        up: () => {player1.down = false}
+      },
+      {
+        key: leftKey, 
+        down: () => { player1.left = true}, 
+        up: () => {player1.left = false}
+      },
+      {
+        key: jumpKey, 
+        down: player1.jump.bind(player1)
+      }
     ]
 
     this.players = this.add.group()
-    this.walls = this.add.group()
 
     this.players.add(player1)
-    this.walls.add(wall1)
   }
 
   update() {
     var {inputs, game, players, walls} = this 
 
     players.callAll("stop")
-    inputs.forEach(doIfPressed)
+    inputs.forEach(doActionForKey)
   }
 }
 
-var doIfPressed = (input) => {
-  if (input.key.isDown) input.fn()
+var noop = () => {}
+
+var doActionForKey = ({key, down, up}) => {
+  var down = down || noop
+    , up = up || noop
+
+  if (key.isDown) down()
+  else up()
 }
